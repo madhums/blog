09e9db395c1f2ef2adaf6a91a7f4f617f0d6ac82
Madhusudhan Srinivasa <madhums8@gmail.com>
2018-10-28 17:00:46 +0100

    add postgres

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const pg = require('pg');
 const app = express();
+const { PORT, DATABASE_URL, NODE_ENV } = process.env;
+const isProduction = NODE_ENV === 'production';
 
-const { PORT } = process.env;
+app.get('/', (req, res) => res.send('Hello world 7'));
+app.get('/pg', (req, res) => {
+  client.query('SELECT $1::text as message', ['Hello world!'], (err, dbres) => {
+    client.end();
+    res.send(err ? err.stack : dbres.rows[0].message);
+  });
+});
 
-app.get('/', (req, res) => res.send('Hello world 6'));
+const client = new pg.Client({
+  connectionString: DATABASE_URL,
+  ssl: isProduction
+});
 
-app.listen(PORT, () => console.log('Express server started on port ', PORT));
+client
+  .connect()
+  .then(start)
+  .catch(e => console.error('connection error', e.stack));
+
+function start() {
+  console.log('Connected to database');
+  app.listen(PORT, () => console.log('Express server started on port ', PORT));
+}
