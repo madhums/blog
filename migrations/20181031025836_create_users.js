bae4b0aaf3b3356df2db86d5083d028e9fdb7ddb
Madhusudhan Srinivasa <madhums8@gmail.com>
2018-10-31 03:54:04 +0100

    add create_users table schema and migration

---
+++ migrations/20181031025836_create_users.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table => {
+    table.increments('id').primary();
+    table.string('first_name');
+    table.string('last_name');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
